e212399e08ee4099527366359666c5a22b3e5900
Blx <itszacktouil@gmail.com>
2023-12-09 16:07:27 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Container } from "@mantine/core";
-import AppRouter from "./router/AppRouter";
+// import AppRouter from "./router/AppRouter";
 import { useDevicesContext } from "./context/DevicesContext";
+import Romoch from "./screens/romoch/Romoch";
 
 export default function App() {
     const {isTabletAndMobile} = useDevicesContext()
     return (
         <Container size={isTabletAndMobile?'xl':'md'} p={0} pb={60}>
-            <AppRouter />
+            <Romoch/>
         </Container>
     );
 }
